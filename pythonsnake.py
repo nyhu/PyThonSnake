e1a502d197124739292b4eeafd8449e729f0728d
nyhu <toussaint.boos@gmail.com>
2017-07-17 01:26:05 +0200

    init core

---
+++ pythonsnake.py
@@ -0,0 +1,21 @@
+"""
+Hearth of the programm:
+    main class is used to load the snake in accordance with the exec environement
+    it takes:
+        size_x
+        size_y
+"""
+import sys
+
+class PyThonSnake():
+    """Only compatible with terminal at the moment"""
+
+    def __init__(self):
+        """..."""
+        pass
+
+    def init_term(self, argv):
+        """..."""
+        if len(sys.argv) > 1:
+            for x in sys.argv:
+                pass
